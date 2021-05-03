45046a206b6aeaae396bfb09f722a43930ece698
RaddatzK <8616693+raddatzk@users.noreply.github.com>
2021-05-03 07:02:06 -0400

    Use custom .zshrc

--- run_once_08_zsh.sh
+++ run_once_08_zsh.sh
@@ -6,6 +6,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-chsh -s /usr/bin/zsh kali
-
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
+chsh -s /usr/bin/zsh kali
